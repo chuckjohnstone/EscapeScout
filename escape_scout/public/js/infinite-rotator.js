06f36e183c60b713f10ac8777cb4c8e7ae7ddbc5
Chuck Johnston <charles.g.johnston@gmail.com>
2013-01-31 17:04:45 -0800

    Extended time between slides and rearranged two.

--- escape_scout/public/js/infinite-rotator.js
+++ escape_scout/public/js/infinite-rotator.js
@@ -8,7 +8,7 @@ $(window).load(function () { //start after HTML, images have loaded
             var initialFadeIn = 1000;
  
             //interval between items (in milliseconds)
-            var itemInterval = 5000;
+            var itemInterval = 10000;
  
             //cross-fade time (in milliseconds)
             var fadeTime = 2500;
